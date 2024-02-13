62e28e3e898ae2679c7f1d5ed4a3b5ca79811c6d
msnetninja <jeffreymin0213@gmail.com>
2024-02-13 11:38:13 -0600

    add Ingredient model and use on the shopping-list component

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,5 @@
+export class Ingredient {
+  // This is a shortcut to access and assign the variables in the declaration.
+  // It does the same as our Recipe model implementation, but shorter. 
+  constructor(public name: string, public amount: number) { }
+}
