dc5befc8086c26e8be00abaeab7303254f918716
msnetninja <jeffreymin0213@gmail.com>
2024-02-13 10:17:55 -0600

    Add Recipe model and populate recipes array with recipe instances

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrl: './recipe-list.component.css'
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is a simple test', 'https://img-s-msn-com.akamaized.net/tenant/amp/entityid/BB1id6na.img')
+  ];
 
+  constructor() {}
+
+  ngOnInit() {
+
+  }
 }
