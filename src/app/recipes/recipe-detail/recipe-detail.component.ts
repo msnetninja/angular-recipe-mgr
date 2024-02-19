26a4ce4db34abf0bdb474782c3c7b6438d2d69a3
msnetninja <jeffreymin0213@gmail.com>
2024-02-19 14:16:12 -0600

    Support edit-recipe route and code clean up

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -14,9 +14,9 @@ export class RecipeDetailComponent {
   id: number;
 
   constructor(private recipeService: RecipeService,
-    private route: ActivatedRoute) {
-
-    }
+              private route: ActivatedRoute,
+              private router: Router) {
+  }
 
   ngOnInit() {
     this.route.params
@@ -30,5 +30,9 @@ export class RecipeDetailComponent {
 
   onAddToShoppingList() {
     this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
+  }
+
+  onEditRecipe() {
+    this.router.navigate(['edit'], { relativeTo: this.route });
   }
 }
